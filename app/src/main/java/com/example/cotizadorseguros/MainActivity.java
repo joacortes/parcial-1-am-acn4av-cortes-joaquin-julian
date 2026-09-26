94f3e79deb8d32b4df9ef0ea7740ebe00b75fc63
Joaquín Cortes <85517684+joacortes@users.noreply.github.com>
2026-09-26 11:24:31 -0300

    refactor: se declararon las variables para los componentes que se usaran y en onCreate conectamos cada variable con el ID definido en el XML

--- app/src/main/java/com/example/cotizadorseguros/MainActivity.java
+++ app/src/main/java/com/example/cotizadorseguros/MainActivity.java
@@ -10,11 +10,45 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button btnPersonaFisica;
+    private Button btnPersonaJuridica;
+    private Button btnAgregarTelefono;
+    private Button btnGuardar;
+    private LinearLayout contenedorDatosTipoPersona;
+    private LinearLayout contenedorTelefonos;
+    private EditText inputEmail;
+    private EditText inputTelefono;
+    private EditText inputCalle;
+    private EditText inputNumero;
+    private EditText inputLocalidad;
+    private String tipoPersonaSeleccionado = "";
+    private ArrayList<String> telefonosAdicionales = new ArrayList<>();
+    private EditText inputNombre;
+    private EditText inputApellido;
+    private EditText inputDni;
+    private EditText inputFechaNacimiento;
+    private EditText inputRazonSocial;
+    private EditText inputCuit;
+    private EditText inputNombreFantasia;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        btnPersonaFisica = findViewById(R.id.btnPersonaFisica);
+        btnPersonaJuridica = findViewById(R.id.btnPersonaJuridica);
+        btnAgregarTelefono = findViewById(R.id.btnAgregarTelefono);
+        btnGuardar = findViewById(R.id.btnGuardar);
+        contenedorDatosTipoPersona = findViewById(R.id.contenedorDatosTipoPersona);
+        contenedorTelefonos = findViewById(R.id.contenedorTelefonos);
+        inputEmail = findViewById(R.id.inputEmail);
+        inputTelefono = findViewById(R.id.inputTelefono);
+        inputCalle = findViewById(R.id.inputCalle);
+        inputNumero = findViewById(R.id.inputNumero);
+        inputLocalidad = findViewById(R.id.inputLocalidad);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
